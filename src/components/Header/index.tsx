1403f7e9385f1ae06880cb9c2cfe3f41101859f3
Guga Villar <grsv.21@gmail.com>
2021-11-16 13:23:42 -0300

    Criação do component header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss';
+
+export const Header = () => {
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <h1>Burguer</h1>
+        <div>
+          <input type="text" placeholder="Digitar pesquisa" />
+          <button>Pesquisar</button>
+        </div>
+      </div>
+    </header>
+  )
+}
